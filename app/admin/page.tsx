0619f817959d8e9028404b72bcf8f94b7fa333da
NucleusBeast <36455332+NucleusBeast@users.noreply.github.com>
2026-01-17 10:19:24 +0100

    First page

---
+++ app/admin/page.tsx
@@ -0,0 +1,9 @@
+export default function AdminPage() {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold">Admin Dashboard</h1>
+
+      <p>Welcome to the admin dashboard of the portfolio website!</p>
+    </div>
+  );
+}
